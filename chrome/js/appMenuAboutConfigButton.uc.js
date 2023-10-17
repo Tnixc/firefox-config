71857fe4dec83e3bb590a072e38671df414b7015
Tnixc <tnixxc@gmail.com>
2023-10-17 19:58:19 -0400

    holy hell that's a lot of changes

---
+++ chrome/js/appMenuAboutConfigButton.uc.js
@@ -0,0 +1,137 @@
+// ==UserScript==
+// @name           App Menu about:config Button
+// @version        1.2.3
+// @author         aminomancer
+// @homepage       https://github.com/aminomancer/uc.css.js
+// @description    Adds an about:config shortcut button to the main app menu panel, under the
+// built-in Settings button. It can open the built-in about:config page, or it can open the
+// old-school about:config page with earthlng's aboutconfig module. (https://github.com/earthlng/aboutconfig)
+// To use that with fx-autoconfig, download ONLY the profile/chrome/utils/aboutconfig folder and
+// place it inside your profile/chrome/resources folder. Then open config.xhtml and find & replace
+// "userchromejs" with "userchrome" and save. Now "chrome://userchrome/content/aboutconfig/config.xhtml"
+// should be the correct URL. By default the script will open to that link, so if you don't have
+// that module installed the button will open to a blank page. If you can't get the module to work
+// or if you just prefer Firefox's built-in page, you can change the constant on line 10 below to
+// "about:config" and it'll open to the same page you'd get if you typed about:config in the address
+// bar. (the URL must be in quotes) That said, typing about:config is already easy enough. The
+// reason I made this script was to make a clean shortcut to reach the old-school page, and in a
+// more central location than a bookmark. FYI I added an icon for this button (and for all the other
+// main app menu buttons too) in uc-app-menu.css
+// @license        This Source Code Form is subject to the terms of the Creative Commons Attribution-NonCommercial-ShareAlike International License, v. 4.0. If a copy of the CC BY-NC-SA 4.0 was not distributed with this file, You can obtain one at http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+// ==/UserScript==
+
+(function () {
+    // user configuration
+    const config = {
+        urlOverride: ""
+        /* the script tries to automatically find earthlng's aboutconfig URL, and if
+    it can't be found, uses the built-in about:config URL instead. if it's
+    unable to find the URL for your particular setup, or if you just want to
+    use the vanilla about:config page, replace this empty string with your
+    preferred URL, in quotes. if you want to use my about:cfg script that
+    registers earthlng's aboutconfig page to about:cfg, and you want the
+    about:config button to take you to about:cfg, then leave this empty. it
+    will automatically use about:cfg if the script exists. if about:cfg
+    doesn't work for you then change the pathOverride in *that* script
+    instead of setting urlOverride in this one. if you changed the address
+    (the "cfg" string) in that script, you'll need to use urlOverride here
+    if you want the button to direct to earthlng's aboutconfig page. so if
+    for example you changed the address to "config2" then change urlOverride
+    above to "about:config2" */
+    };
+
+    function findAboutConfig() {
+        if (config.urlOverride) return config.urlOverride;
+
+        if (
+            Components.manager
+                .QueryInterface(Ci.nsIComponentRegistrar)
+                .isContractIDRegistered(
+                    "@mozilla.org/network/protocol/about;1?what=cfg"
+                )
+        ) {
+            return "about:cfg";
+        }
+
+        let dir = Services.dirsvc.get("UChrm", Ci.nsIFile);
+        let appendFn = (nm) => dir.append(nm);
+
+        // fx-autoconfig
+        ["resources", "aboutconfig", "config.xhtml"].forEach(appendFn);
+        if (dir.exists()) {
+            return "chrome://userchrome/content/aboutconfig/config.xhtml";
+        }
+
+        // earthlng's loader
+        dir = Services.dirsvc.get("UChrm", Ci.nsIFile);
+        ["utils", "aboutconfig", "config.xhtml"].forEach(appendFn);
+        if (dir.exists()) {
+            return "chrome://userchromejs/content/aboutconfig/config.xhtml";
+        }
+
+        // xiaoxiaoflood's loader
+        dir = Services.dirsvc.get("UChrm", Ci.nsIFile);
+        ["utils", "aboutconfig", "aboutconfig.xhtml"].forEach(appendFn);
+        if (dir.exists()) {
+            return "chrome://userchromejs/content/aboutconfig/aboutconfig.xhtml";
+        }
+
+        // no about:config replacement found
+        return "about:config";
+    }
+
+    async function createButton() {
+        // get fluent file for AboutConfig page
+        const configStrings = await new Localization(
+            ["toolkit/about/config.ftl"],
+            true
+        );
+        // localize the "Advanced Preferences" string
+        const advancedPrefsLabel = await configStrings.formatValue([
+            "about-config-page-title"
+        ]);
+        const { mainView } = PanelUI;
+        const doc = mainView.ownerDocument;
+        const settingsButton =
+            doc.getElementById("appMenu-settings-button") ??
+            doc.getElementById("appMenu-preferences-button");
+        const prefsButton = doc.createXULElement("toolbarbutton");
+
+        prefsButton.preferredURL = findAboutConfig();
+        for (const [key, val] of Object.entries({
+            id: "appMenu-advanced-settings-button",
+            class: "subviewbutton",
+            label: advancedPrefsLabel,
+            oncommand: `openTrustedLinkIn(this.preferredURL, gBrowser.currentURI.spec === AboutNewTab.newTabURL || gBrowser.currentURI.spec === HomePage.get(window) ? "current" : "tab")`
+        })) {
+            prefsButton.setAttribute(key, val);
+        }
+        // place after the built-in "Settings" button
+        settingsButton.after(prefsButton);
+    }
+
+    function init() {
+        PanelMultiView.getViewNode(
+            document,
+            "appMenu-multiView"
+        ).addEventListener("ViewShowing", createButton, { once: true });
+    }
+
+    if (gBrowserInit.delayedStartupFinished) {
+        init();
+    } else {
+        let delayedListener = (subject, topic) => {
+            if (
+                topic == "browser-delayed-startup-finished" &&
+                subject == window
+            ) {
+                Services.obs.removeObserver(delayedListener, topic);
+                init();
+            }
+        };
+        Services.obs.addObserver(
+            delayedListener,
+            "browser-delayed-startup-finished"
+        );
+    }
+})();
---
+++ chrome/js/appMenuMods.uc.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name           App Menu Mods
+// @version        1.4.2
+// @author         aminomancer
+// @homepage       https://github.com/aminomancer/uc.css.js
+// @description    Makes some minor modifications to the app menu (the popup
+// opened by clicking the hamburger button on the far right of the navbar). It
+// adds a restart button to the app menu and it adds a separator under the
+// "Manage Account" button in the profile/account panel. I'll continue adding
+// more mods to this script as I think of them.
+// @license        This Source Code Form is subject to the terms of the Creative Commons Attribution-NonCommercial-ShareAlike International License, v. 4.0. If a copy of the CC BY-NC-SA 4.0 was not distributed with this file, You can obtain one at http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+// ==/UserScript==
+
+(function () {
+    class AppMenuMods {
+        constructor() {
+            PanelUI._initialized ||
+                PanelUI.init(shouldSuppressPopupNotifications);
+            PanelUI.mainView.addEventListener("ViewShowing", this, {
+                once: true
+            });
+            this.addSeparatorToAccountPanel();
+            this.fixSyncSubviewButtonAlignment();
+        }
+        static create(aDoc, tag, props, isHTML = false) {
+            let el = isHTML
+                ? aDoc.createElement(tag)
+                : aDoc.createXULElement(tag);
+            for (let prop in props) {
+                el.setAttribute(prop, props[prop]);
+            }
+            return el;
+        }
+        static sleep(ms) {
+            return new Promise((resolve) => setTimeout(resolve, ms));
+        }
+        async generateStrings() {
+            if (!this.strings) {
+                this.strings = await new Localization(
+                    ["toolkit/about/aboutSupport.ftl"],
+                    true
+                );
+            }
+            await AppMenuMods.sleep(1);
+            return this.strings;
+        }
+        get fxaPanelView() {
+            return PanelMultiView.getViewNode(document, "PanelUI-fxa");
+        }
+        async handleEvent(_e) {
+            let strings = await this.generateStrings();
+            this.addRestartButton(strings);
+        }
+        addSeparatorToAccountPanel() {
+            this.manageAccountSeparator =
+                this.fxaPanelView.ownerDocument.createXULElement(
+                    "toolbarseparator"
+                );
+            this.fxaPanelView
+                .querySelector("#fxa-manage-account-button")
+                .after(this.manageAccountSeparator);
+        }
+        async addRestartButton(strings) {
+            let restartButton = AppMenuMods.create(document, "toolbarbutton", {
+                id: "appMenu-restart-button2",
+                class: "subviewbutton",
+                label: await strings.formatValue(["restart-button-label"]),
+                oncommand: `if (event.shiftKey || (AppConstants.platform == "macosx" ? event.metaKey : event.ctrlKey)) Services.appinfo.invalidateCachesOnRestart(); setTimeout(() => Services.startup.quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit), 300); PanelMultiView.forNode(this.closest("panelmultiview")).hidePopup(); event.preventDefault();`,
+                onclick: `if (event.button === 0) return; Services.appinfo.invalidateCachesOnRestart(); setTimeout(() => Services.startup.quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit), 300); PanelMultiView.forNode(this.closest("panelmultiview")).hidePopup(); event.preventDefault();`
+            });
+            let exitButton = document.getElementById("appMenu-quit-button2");
+            if (exitButton) {
+                exitButton.before(restartButton);
+            } else {
+                PanelUI.mainView
+                    .querySelector(".panel-subview-body")
+                    .appendChild(restartButton);
+            }
+        }
+        fixSyncSubviewButtonAlignment() {
+            eval(
+                `gSync.populateSendTabToDevicesView = function ` +
+                    gSync.populateSendTabToDevicesView
+                        .toSource()
+                        .replace(/^populateSendTabToDevicesView/, ``)
+                        .replace(
+                            /item.setAttribute\(\"align\", \"start\"\);/,
+                            ``
+                        )
+            );
+        }
+    }
+
+    if (gBrowserInit.delayedStartupFinished) {
+        new AppMenuMods();
+    } else {
+        let delayedListener = (subject, topic) => {
+            if (
+                topic == "browser-delayed-startup-finished" &&
+                subject == window
+            ) {
+                Services.obs.removeObserver(delayedListener, topic);
+                new AppMenuMods();
+            }
+        };
+        Services.obs.addObserver(
+            delayedListener,
+            "browser-delayed-startup-finished"
+        );
+    }
+})();
